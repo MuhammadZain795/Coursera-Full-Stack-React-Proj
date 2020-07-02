0fc9b2f2748dc1b7d5a4294f66fb572d2d3e5a31
MuhammadZain795 <bsef17m030@pucit.edu.pk>
2020-07-02 07:06:47 +0500

    React redux form revisited

--- confusion/src/components/ContactComponent.js
+++ confusion/src/components/ContactComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Breadcrumb, BreadcrumbItem,
     Button, Row, Col, Label } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -16,14 +16,15 @@ class Contact extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         
     }
-    handleSubmit(event) {
-        console.log('Current State is: ' + JSON.stringify(this.state));
-        alert('Current State is: ' + JSON.stringify(this.state));
-        event.preventDefault();
-    }
+    // handleSubmit(event) {
+    //     console.log('Current State is: ' + JSON.stringify(this.state));
+    //     alert('Current State is: ' + JSON.stringify(this.state));
+    //     event.preventDefault();
+    // }
     handleSubmit(values) {
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
+        this.props.resetFeedbackForm();
         // event.preventDefault();
     }
     
@@ -72,7 +73,7 @@ class Contact extends Component {
                       <h3>Send us your Feedback</h3>
                    </div>
                     <div className="col-12 col-md-9">
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                     <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -162,7 +163,7 @@ class Contact extends Component {
                                 </Col>
                             </Row>
                             
-                        </LocalForm>
+                        </Form>
                         
                     </div>
                </div>
