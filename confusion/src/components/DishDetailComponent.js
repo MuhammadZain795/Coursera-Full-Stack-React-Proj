8ec4321da3dae1ba65379ba29b944680380ef26c
MuhammadZain795 <bsef17m030@pucit.edu.pk>
2020-07-02 16:37:03 +0500

    fetch post comment

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -23,8 +23,8 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
 		this.toggleModal();
-		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
-	}
+		this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
+		}
 
 	toggleModal() {
 		this.setState({
@@ -101,7 +101,7 @@ function RenderDish({dish}) {
 		);
 	}
 
-	function RenderComments({comments, addComment, dishId}) {
+	function RenderComments({comments, postComment, dishId}) {
 		const commentList = comments.map((comment) => {
 			return (
 				<li className="mb-1" key={comment.id}>
@@ -118,7 +118,7 @@ function RenderDish({dish}) {
 				<ul className="list-unstyled">
 					{commentList}
 				</ul>
-				<CommentForm dishId={dishId} addComment={addComment} />
+				<CommentForm dishId={dishId} postComment={postComment} />
 				</div>
 				);
 		} else {
@@ -162,7 +162,7 @@ const DishDetail = (props) => {
 				<div className="row">
 					<RenderDish dish={props.dish} />
 					<RenderComments comments={props.comments}
-        				addComment={props.addComment}
+        				postComment={props.postComment}
         				dishId={props.dish.id}
       />
 				</div>
